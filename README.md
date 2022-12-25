c0ca926d431f3a75c6d7d2296c0e39bf5855d71e
faroukhamadi <faroukhamadi1@outlook.com>
2022-12-25 03:01:23 +0100

    add plotting and docs for logistic regression

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # logistic-regression
+
+This is a simple implementation of logistic regression in Python. It uses a random x1 and x2 to generate a random y. It then uses the x1 and x2 to predict the y and Finally the sigmoid function to predict the y.Newton-Raphson is the predictor to find the optimal theta for the cost function.
+
+## Usage
+
+Run the [notebook](./logistic_regression_notebook.ipynb)  in Jupyter Notebook or Jupyter Lab.
+
+## implementation details
+
+You can find the implementation details in the logistic_regression.py file.
--- logistic_regression.py
+++ logistic_regression.py
@@ -5,13 +5,22 @@ from linear_model import LinearModel
 
 
 class LogisticRegression(LinearModel):
-    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
-        """Run Newton's method to minimize J(theta) for logistic regression
+    """Logistic regression with Newton's Method as the solver.
+
+    Example usage:
+        > clf = LogisticRegression()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        """Run Newton's Method to minimize J(theta) for logistic regression.
 
         Args:
-            x (np.ndarray): Training example inputs. shape (m, n)
-            y (np.ndarray): Training example labels (The thing we're predicting). shape (m,)
+            x: Training example inputs. Shape (m, n).
+            y: Training example labels. Shape (m,).
         """
+        # *** START CODE HERE ***
 
         # Initialize theta based on number of features
         m, n = x.shape
@@ -22,30 +31,44 @@ class LogisticRegression(LinearModel):
             # Save old theta to compare later
             old_theta = np.copy(self.theta)
 
-            # Compute Hessian Matrix
+            # Compute Hessian Matri
+            # '../assets/logistic_hypothesis_function.png'
             h_x = 1 / (1 + np.exp(-x.dot(self.theta)))
             H = (x.T * h_x * (1 - h_x)).dot(x) / m
-            gradient_J_theta = x.T.dot(h_x - y) // m
+            gradient_J_theta = x.T.dot(h_x - y) / m
 
-            # Update Theta
+            # Update theta
             self.theta -= np.linalg.inv(H).dot(gradient_J_theta)
 
             # End training if norm of theta_k - theta_k-1 < epsilon
             if np.linalg.norm(self.theta - old_theta, ord=1) < self.eps:
                 break
+        # *** END CODE HERE ***
 
-    def predict(self, x: np.ndarray) -> list[int]:
+    def predict(self, x) -> list[int]:
         """Make a prediction given new inputs x.
 
         Args:
-            x: Inputs of shape (m,n).
+            x: Inputs of shape (m, n).
 
         Returns:
             Outputs of shape (m,).
         """
+        # *** START CODE HERE ***
         y_preds: list[int] = []
-        for elem in x[0]:
+        for elem in x:
             y = round(util.sigmoid(self.theta.dot(elem)))
             y_preds.append(y)
-
         return y_preds
+        # *** END CODE HERE ***
+
+
+x_train, y_train = util.load_dataset('data/ds1_train.csv', add_intercept=True)
+
+x_valid, y_valid = util.load_dataset('data/ds1_valid.csv', add_intercept=True)
+
+clf = LogisticRegression(theta_0=0)
+clf.fit(x_train, y_train)
+preds = clf.predict(x_valid)
+# preds = clf.predict(x_train)
+print('preds are: ', preds)
